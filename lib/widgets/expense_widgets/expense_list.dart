606791636d82510143831e269cef9f2455b628c8
KovaKreative <eric.kovalevskyy@gmail.com>
2025-05-28 12:56:30 -0400

    Create expense list

--- lib/widgets/expense_widgets/expense_list.dart
+++ lib/widgets/expense_widgets/expense_list.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/expense_widgets/expense_item.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -10,7 +11,7 @@ class ExpenseList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, i) => Text(expenses[i].title),
+      itemBuilder: (ctx, i) => ExpenseItem(expenses[i]),
     );
   }
 }
